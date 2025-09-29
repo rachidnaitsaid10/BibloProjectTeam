21a895f2f9a9d583c38b698756b58d978d0270f9
rachidnaitsaid10 <rachidnaitsaid10@gmail.com>
2025-09-29 10:15:56 +0100

    Add Search and books functions in main

--- main.py
+++ main.py
@@ -1,16 +1,57 @@
-# This is a sample Python script.
+from Data import *
+from books import *
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print(utilisateurs)
+print(aime_livres)
+print(livres)
+print("-"*220)
+
+print(SortingData(livres))
+Ancien(livres)
+Récent(livres)
+CreationDict(aime_livres)
+PaginationList(utilisateurs)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def Search_Livre(MyList):
+    Nom_Livre = input("Nom de la livre: ")
+    if Nom_Livre != "":
+        MonLivre = list(filter(lambda x: x["titre"] == Nom_Livre, MyList))
+        print(MonLivre)
+    else :
+        print("Invalid Input")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def Search_Users(MyList):
+    Nom_User = input("Nom de la user: ")
+    if Nom_User != "":
+        MonUser = list(filter(lambda x: x[1] == Nom_User, MyList))
+        print(MonUser)
+    else :
+        print("Invalid Input")
+
+
+
+def menu():
+    Choix = 0
+    while Choix != 3 or Choix > 3 or Choix != "":
+        print("Bienvenue !!!!!")
+        print("-"*15)
+        print("1-Pour Rechercher livre :")
+        print("2-Pour Rechercher utilisateurs :")
+        print("3-Quitter   :")
+        print("-"*15)
+        Choix = int(input("Choix entre 1 et 3 : "))
+
+        if Choix == 1:
+            Search_Livre(livres)
+        if Choix == 2:
+            Search_Users(utilisateurs)
+
+# menu()
+
+
+
+
+
