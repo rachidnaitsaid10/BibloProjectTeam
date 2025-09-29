433a50ad9d6da4493911569934d6c233a2b3dfda
rachidnaitsaid10 <rachidnaitsaid10@gmail.com>
2025-09-29 10:07:05 +0100

    Add Data.py

---
+++ Data.py
@@ -0,0 +1,19 @@
+utilisateurs = [
+    (1, "Alice", "Dupont", 25),
+    (2, "Bob", "Martin", 17),
+    (3, "Clara", "Durand", 32),
+    (4, "David", "Petit", 20)
+]
+
+livres = [
+    {"titre": "1984", "auteur": "George Orwell", "année": 1949},
+    {"titre": "Le Petit Prince", "auteur": "Antoine de Saint-Exupéry", "année": 1943},
+    {"titre": "Harry Potter", "auteur": "J.K. Rowling", "année":1997}
+]
+
+aime_livres = [
+    (1, "1984"),
+    (1, "Le Petit Prince"),
+    (3, "Harry Potter"),
+    (4, "1984")
+]
